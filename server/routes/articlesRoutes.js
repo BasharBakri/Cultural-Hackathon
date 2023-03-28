122a47c9ed17bcd07938dd61acc8d8d80a0d5c8e
Eyal-donut <LilaEyal@gmail.com>
2023-03-28 18:43:32 +0300

    backend_edits

--- server/routes/articlesRoutes.js
+++ server/routes/articlesRoutes.js
@@ -1,16 +1,18 @@
 import express from "express";
+import {getAllArticles, getOneArticle, postOneArticle, updateOneArticle, deleteOneArticle} from '../controllers/articleControllers.js'
 
 const articlesRouter = express.Router();
 
 articlesRouter
   .route("/")
-  // .get(getArticles)
-  // .post(createArticle)
+  .get(getAllArticles)
+  .post(postOneArticle)
   // .delete(deleteArticles);
 
 articlesRouter
   .route("/:id")
-  // .get(getArticle)
-  // .delete(deleteArticle);
+  .get(getOneArticle)
+  .put(updateOneArticle)
+  .delete(deleteOneArticle);
 
 export default articlesRouter;
