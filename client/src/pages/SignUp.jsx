4426653aa27d31088ec1d9cee045882c67bdb10e
Nita1001 <devinedevops@gmail.com>
2023-03-28 15:48:36 +0300

    signUp page

---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "../components/Form";
+
+const SignUp = () => {
+    return (
+        <div>
+            <h1>Sign Up</h1>
+            <Form type="signUp" />
+        </div>
+    );
+};
+
+export default SignUp;
