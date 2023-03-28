a363201355bdadd3a492784d1f97e12af86b2d34
momotan <momotaha039@gmail.com>
2023-03-28 15:38:32 +0300

    add some fileds and background to article

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -3,11 +3,16 @@ import React from 'react'
 function Article({article}) {
   return (
     <div className="article">
+        <div className="detials">
         <h2>{article.title}</h2>
         <p>{article.description}</p>
         <h3>Category:{article.category}</h3>
         <h3>Published:{article.date}</h3>
         <h3>Answer:{article.answer}</h3>
+        <h3>Author:{article.author}</h3>
+        <h3>Source:{article.source}</h3>
+        </div>
+        <div style={{backgroundImage:'url("/new.jpg")'}} className="image"></div>
     </div>
   )
 }
