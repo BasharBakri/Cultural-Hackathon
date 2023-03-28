a4f3a5f77220ae26fffc63c0af6ad6c85fbc3954
Nita1001 <devinedevops@gmail.com>
2023-03-28 15:47:45 +0300

    signUp login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header'
-import {RouterProvider,createBrowserRouter}from 'react-router-dom'
-import Root from './pages/Root'
-import Home from './pages/Home'
-import Admin from './pages/admin'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./pages/Root";
+import Home from "./pages/Home";
+import Admin from "./pages/admin";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+
 function App() {
-  const router=new createBrowserRouter([
-    {
-      path:'/',
-      element:<Root/>,
-      children:[
+    const router = new createBrowserRouter([
         {
-          path:'/',
-          element:<Home/>
+            path: "/",
+            element: <Root />,
+            children: [
+                {
+                    path: "/",
+                    element: <Home />,
+                },
+                {
+                    path: "/admin",
+                    element: <Admin />,
+                },
+                {
+                    path: "/signup",
+                    element: <SignUp />,
+                },
+                {
+                    path: "/login",
+                    element: <Login />,
+                },
+            ],
         },
-        {
-          path:'/admin',
-          element:<Admin/>
-        }
-      ]
-    }
-  ])  
-  return (
-    <div className="App">
-        <RouterProvider router={router}/>
-    </div>
-  )
+    ]);
+    return (
+        <div className="App">
+            <RouterProvider router={router} />
+        </div>
+    );
 }
 
-export default App
+export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,17 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 function Header() {
-  return (
-    <header>
-        <div className="logo">Cultural Hackaton</div>
-        <nav>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/admin'>Admin</NavLink>
-        </nav>
-    </header>
-  )
+    return (
+        <header>
+            <div className="logo">Cultural Hackaton</div>
+            <nav>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/admin">Admin</NavLink>
+                <NavLink to="/signup">Sign Up</NavLink>
+                <NavLink to="/login">Login</NavLink>
+            </nav>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
