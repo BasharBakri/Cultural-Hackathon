d27209e4cdcc847e28e831020f82fcd341bec560
Nita1001 <devinedevops@gmail.com>
2023-03-28 15:48:30 +0300

    login page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "../components/Form";
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <Form type="login" />
+        </div>
+    );
+};
+
+export default Login;
