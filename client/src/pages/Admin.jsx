df023d9519c0c800b6530b0a78a2281d370fc7a6
momotan <momotaha039@gmail.com>
2023-03-28 20:27:23 +0300

    add admin component

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from "../App";
+import Article from "../components/Article";
+import { getArticles } from "../utils/articles.mjs";
 function Admin() {
+    const appContext=useContext(AppContext)
+    const [articles,setArticles]=useState([])
+    const refreshArticles=(cat)=>{
+      appContext.loading.play()
+      getArticles().then((data)=>{
+        setArticles(data)
+        appContext.loading.pause()
+      })
+  }
+  useEffect(()=>{
+      refreshArticles()
+  },[])
+
   return (
-    <div>Admin Page</div>
-  )
+    <div className="admin">
+      <div className="articles">
+        {articles.map((a, i) => {
+          return <Article key={i} article={a} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Admin
+export default Admin;
