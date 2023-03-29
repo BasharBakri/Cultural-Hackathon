7e1d6a1d43542913248254543c22f73eb29f0ce6
MOHAMMED <momotaha039@gmail.com>
2023-03-29 17:32:02 +0300

    bind it to the server and fix filter by categories

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -1,21 +1,37 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { FaCalendarTimes } from "react-icons/fa";
 import { MdCategory, MdOutlineManageSearch } from "react-icons/md";
 import { HiNewspaper } from "react-icons/hi";
-import { changeArticleStatus } from "../utils/articles.mjs";
+import { changeArticle } from "../utils/articles.mjs";
 import { AppContext } from "../contexts/AppContext.jsx";
 
 function Article({ article, isAdmin }) {
   const appContext=useContext(AppContext)
+  const catInputRef=useRef()
   const onChnageActivityArticle=()=>{
     appContext.loading.play()
-    changeArticleStatus(article).then((data)=>{
+    article.isInHomePage=!article.isInHomePage;
+    changeArticle(article).then((data)=>{
       appContext.loading.pause()
-      alert('Status Article changed to:'+data)
+      alert('Status Article changed to:'+data.isInHomePage)
     }).catch(err=>{
       alert(err+'')
     })
   }
+
+  const onChnageArticle=()=>{
+    article.category=catInputRef.current.value
+    debugger
+    appContext.loading.play()
+    changeArticle(article).then((data)=>{
+      appContext.loading.pause()
+      alert('cat Article changed to:'+data.category)
+    }).catch(err=>{
+      alert(err+'')
+    })
+  }
+
+
   return (
     <div className="article">
       <div className="detials">
@@ -26,7 +42,7 @@ function Article({ article, isAdmin }) {
           </h3>
           <h3 className="icon">
             <MdCategory size={25} />
-            {article.category}
+            {article.category??'no category'}
           </h3>
           <h3 className="icon">
             <HiNewspaper size={25} />
@@ -35,24 +51,26 @@ function Article({ article, isAdmin }) {
         </div>
         <h2>{article.title}</h2>
         <p>{article.description}</p>
-        <a className="src" href="#">
+        <a className="src" href={article.articleURL}>
           <h3>
             Source
-            <MdOutlineManageSearch size={30} />: {article.source}
+            <MdOutlineManageSearch size={30} />: {article.websiteName}
           </h3>
         </a>
         {isAdmin && (
           <div className="switch">
           <div className="data">
           <h2>Active:</h2>
-            <input onChange={()=>onChnageActivityArticle()}  defaultChecked={article.isActive} type="checkbox" id={article.id} />
+            <input onChange={()=>onChnageActivityArticle()}  defaultChecked={article.isInHomePage} type="checkbox" id={article.id} />
             <label htmlFor={article.id}></label>
+            <button onClick={()=>onChnageArticle()} className="catBtn">save cat</button>
+            <input ref={catInputRef} type="text" placeholder={article.category} />
           </div>
           </div>
         )}
       </div>
       <div
-        style={{ backgroundImage: 'url("/new.jpg")' }}
+        style={{ backgroundImage: `url(${article.imageUrl})` }}
         className="image"
       ></div>
     </div>
--- client/src/utils/articles.mjs
+++ client/src/utils/articles.mjs
@@ -1,91 +1,43 @@
-const articles = [
-  {
-    id:1,
-title: "New Study Finds that Coffee May Reduce Risk of Heart Disease",
-    description:
-      "A new study published in the American Journal of Cardiology suggests that drinking coffee may lower the risk of heart disease.",
-    date: "2022-05-12",
-    answer: "positive",
-    image: "https://example.com/images/coffee-heart.jpg",
-    category: "Health",
-    author: "John Smith",
-    isActive: true,
-    source: "The New York Times",
-  },
-  {
-    id:2,
-title: "Scientists Discover New Gene Linked to Alzheimer's Disease",
-    description:
-      "Researchers at the University of California have identified a new gene that appears to play a key role in the development of Alzheimer's disease.",
-    date: "2022-06-18",
-    answer: "negative",
-    image: "https://example.com/images/alzheimers-gene.jpg",
-    category: "Science",
-    author: "Jane Doe",
-    isActive: true,
-    source: "Science Daily",
-  },
-  {
-    id:3,
-    title:
-      "New Survey Shows Majority of Americans Believe Climate Change is Real",
-    description:
-      "A new survey conducted by the Pew Research Center has found that 65% of Americans believe that climate change is happening and is largely caused by human activity.",
-    date: "2022-07-23",
-    answer: "positive",
-    image: "https://example.com/images/climate-change.jpg",
-    category: "Environment",
-    author: "Sarah Johnson",
-    isActive: false,
-    source: "CNN",
-  },
-  {
-    id:4,
-title: "Study Shows Link Between Social Media Use and Depression in Teens",
-    description:
-      "A new study published in the Journal of Adolescent Health has found that excessive social media use is linked to an increased risk of depression in teenagers.",
-    date: "2022-09-01",
-    answer: "negative",
-    image: "https://example.com/images/social-media-depression.jpg",
-    category: "Psychology",
-    author: "Robert Lee",
-    isActive: false,
-    source: "Medical News Today",
-  },
-];
 export const getArticles = async (cat) => {
-  return new Promise((res) => {
-    setTimeout(() => {
-      if (cat) res(articles.filter((f) => f.category === cat));
-      else res(articles);
-    }, 1000);
-  });
+  const res=await fetch('https://hackathon.cyclic.app/api/v1/news/articles').then((res)=>res.json())
+  return res.data
+  // return new Promise((res) => {
+  //   setTimeout(() => {
+  //     if (cat) res(articles.filter((f) => f.category === cat));
+  //     else res(articles);
+  //   }, 1000);
+  // });
 };
 
 export const getSelectedArticles = async (cat) => {
-  return new Promise((res) => {
-    setTimeout(() => {
-      if(cat)
-      res(articles.filter((f) => f.isActive && cat===f.category));
-      else
-      res(articles.filter((f) => f.isActive));
-    }, 1000);
-  });
+  const data=await getArticles();
+  return data.filter(f=>f.isInHomePage)
+  // return new Promise((res) => {
+  //   setTimeout(() => {
+  //     if(cat)
+  //     res(articles.filter((f) => f.isActive && cat===f.category));
+  //     else
+  //     res(articles.filter((f) => f.isActive));
+  //   }, 1000);
+  // });
 };
 
-export const changeArticleStatus=(article)=>{
-  return new Promise((res)=>{
-    setTimeout(() => {
-       res(!article.isActive)
-    }, 3000);
-  })
-} 
+export const changeArticle=async(article)=>{
 
-export const getCategories = () => {
-  return new Promise((res) => {
-    setTimeout(() => {
-      const cats = articles.map((a) => a.category);
-      res(cats);
-    }, 2000);
-  });
-};
+  const res=await fetch('https://hackathon.cyclic.app/api/v1/news/articles/'+article._id,{
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "Put",
+    body:JSON.stringify(article)
+  }).then(res=>res.json())
+  debugger
+  return res.data;
+}
+
+
+export const getCategories = async () => {
+  const data=await getArticles();
+  const cats=data.map(f=>f.isInHomePage?f.category:'').filter(f=>f?f:'')
+  return [...new Set(cats)]
+  }
