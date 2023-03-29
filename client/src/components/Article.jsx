f0463ef92c9a1dc36cc6f2f46548ec1e65731020
momotan <momotaha039@gmail.com>
2023-03-29 03:28:39 +0300

    add activity field and id also handel changing status

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -1,21 +1,62 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from "react";
+import { FaCalendarTimes } from "react-icons/fa";
+import { MdCategory, MdOutlineManageSearch } from "react-icons/md";
+import { HiNewspaper } from "react-icons/hi";
+import { changeArticleStatus } from "../utils/articles.mjs";
+import { AppContext } from "../contexts/AppContext.jsx";
 
-function Article({article,isAdmin}) {
+function Article({ article, isAdmin }) {
+  const appContext=useContext(AppContext)
+  const onChnageActivityArticle=()=>{
+    appContext.loading.play()
+    changeArticleStatus(article).then((data)=>{
+      appContext.loading.pause()
+      alert('Status Article changed to:'+data)
+    }).catch(err=>{
+      alert(err+'')
+    })
+  }
   return (
     <div className="article">
-        <div className="detials">
+      <div className="detials">
+        <div className="top">
+          <h3 className="icon">
+            <FaCalendarTimes size={25} />
+            {article.date}
+          </h3>
+          <h3 className="icon">
+            <MdCategory size={25} />
+            {article.category}
+          </h3>
+          <h3 className="icon">
+            <HiNewspaper size={25} />
+            {article.author}
+          </h3>
+        </div>
         <h2>{article.title}</h2>
         <p>{article.description}</p>
-        <h3>Category:{article.category}</h3>
-        <h3>Published:{article.date}</h3>
-        <h3>Answer:{article.answer}</h3>
-        <h3>Author:{article.author}</h3>
-        <h3>Source:{article.source}</h3>
-        <button>Active:No</button>
-        </div>
-        <div style={{backgroundImage:'url("/new.jpg")'}} className="image"></div>
+        <a className="src" href="#">
+          <h3>
+            Source
+            <MdOutlineManageSearch size={30} />: {article.source}
+          </h3>
+        </a>
+        {isAdmin && (
+          <div className="switch">
+          <div className="data">
+          <h2>Active:</h2>
+            <input onChange={()=>onChnageActivityArticle()}  defaultChecked={article.isActive} type="checkbox" id={article.id} />
+            <label htmlFor={article.id}></label>
+          </div>
+          </div>
+        )}
+      </div>
+      <div
+        style={{ backgroundImage: 'url("/new.jpg")' }}
+        className="image"
+      ></div>
     </div>
-  )
+  );
 }
 
-export default Article
+export default Article;
